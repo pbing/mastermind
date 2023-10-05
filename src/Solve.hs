c5ddf74236072febedc6ab0e12e504dc1aa3fb61
Bernd Beuster <bernd.beuster@gmail.com>
2023-10-05 18:35:09 +0200

    Simplify solver

--- src/Solve.hs
+++ src/Solve.hs
@@ -5,26 +5,26 @@ import Mastermind
 -- Brute force, ignore the answers and go through all possible states.
 -- This takes 648.5 iterations on average.
 solve1 :: [Peg] -> [Peg] -> ([Peg], Int)
-solve1 _ secret = solve1' (head allPossibilities) secret (tail allPossibilities) 1
+solve1 _ secret = solve1' (head allPossibilities) (tail allPossibilities) 1
   where
-    solve1' :: [Peg] -> [Peg] -> [[Peg]] -> Int -> ([Peg], Int)
-    solve1' g s legal acc
+    solve1' :: [Peg] -> [[Peg]] -> Int -> ([Peg], Int)
+    solve1' g legal acc
       | solved ans = (g, acc)
-      | otherwise = solve1' g' s legal'  (acc + 1)
+      | otherwise = solve1' g' legal' (acc + 1)
       where
-        ans = answers g s
+        ans = answers g secret
         g' = head legal
         legal' = tail legal
 
 solve2 :: [Peg] -> [Peg] -> ([Peg], Int)
-solve2 guess secret = solve2' guess secret allPossibilities 1
+solve2 guess secret = solve2' guess allPossibilities 1
   where
-    solve2' :: [Peg] -> [Peg] -> [[Peg]] -> Int -> ([Peg], Int)
-    solve2' g s legal acc
+    solve2' :: [Peg] -> [[Peg]] -> Int -> ([Peg], Int)
+    solve2' g legal acc
       | solved ans = (g, acc)
-      | otherwise = solve2' g' s legal' (acc + 1)
+      | otherwise = solve2' g' legal' (acc + 1)
       where
-        ans = answers g s
+        ans = answers g secret
         g' = head legal'
         legal' = legalPossibilities (g, ans) legal
 
